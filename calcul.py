5f822403bd6040dc088e2e537d0650a107c6f206
Tojomana <tojomananarandriantenaina@gmail.com>
2024-08-08 12:15:56 -0700

    lasa zay

---
+++ calcul.py
@@ -0,0 +1,5 @@
+# Calcul de la somme de deux nombres
+a = 5
+b = 3
+somme = a + b
+print("La somme de", a, "et", b, "est:", somme)
---
+++ test_calcul.py
@@ -0,0 +1,8 @@
+# test_calcul.py
+
+def somme(a, b):
+    return a + b
+
+def test_somme():
+    assert somme(5, 3) == 8
+    
